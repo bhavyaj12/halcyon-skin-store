7aeb6c1be01ff58867cf3f6b86823f565edecb64
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-03-29 00:25:46 +0530

    chore: added product images and exports

---
+++ src/assets/index.js
@@ -0,0 +1,37 @@
+import Sunscreen1 from "./images/ProductImg/sunscreen1.jpg";
+import Sunscreen2 from "./images/ProductImg/sunscreen2.jpg";
+import Sunscreen3 from "./images/ProductImg/sunscreen3.jpg";
+import Serum1 from "./images/ProductImg/serum1.jpg";
+import Serum2 from "./images/ProductImg/serum2.jpg";
+import Serum3 from "./images/ProductImg/serum3.jpg";
+import Moisturizer1 from "./images/ProductImg/moist1.jpg";
+import Moisturizer2 from "./images/ProductImg/moist2.jpg";
+import Moisturizer3 from "./images/ProductImg/moist3.jpg";
+import Cleanser1 from "./images/ProductImg/cleanser1.jpg";
+import Cleanser2 from "./images/ProductImg/cleanser2.jpg";
+import Cleanser3 from "./images/ProductImg/cleanser3.jpg";
+import Bodycare1 from "./images/ProductImg/body1.jpg";
+import Bodycare2 from "./images/ProductImg/body2.jpg";
+import Bodycare3 from "./images/ProductImg/body3.jpg";
+import Bodycare4 from "./images/ProductImg/body4.jpg";
+import Bodycare5 from "./images/ProductImg/body5.jpg";
+
+export {
+  Sunscreen1,
+  Sunscreen2,
+  Sunscreen3,
+  Serum1,
+  Serum2,
+  Serum3,
+  Moisturizer1,
+  Moisturizer2,
+  Moisturizer3,
+  Cleanser1,
+  Cleanser2,
+  Cleanser3,
+  Bodycare1,
+  Bodycare2,
+  Bodycare3,
+  Bodycare4,
+  Bodycare5
+};
