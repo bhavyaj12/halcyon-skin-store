bc2143d48073bd1700b0af1ef818a7363c75dfa4
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-04-01 18:57:58 +0530

    feat: added cart page

--- src/pages/cart-page/CartPage.jsx
+++ src/pages/cart-page/CartPage.jsx
@@ -1,3 +1,37 @@
+import { Link } from "react-router-dom";
+import { CartCard, CartPrice } from "../../components";
+import { useCart } from "../../contexts";
+import "../../styles/main.css";
+import "./cart.css";
+
 export default function CartPage() {
-    return <h1>This is Cart Page</h1>
+  const { cartState } = useCart();
+  return (
+    <main className="cart-main-container">
+      <div className="cart-heading container-center my-8">
+        <h4>My Cart</h4>
+      </div>
+      <section className="cart-cards-container">
+        <div className="cart-items">
+          {cartState.cart.length > 0 ? (
+            cartState.cart.map((item) => {
+              return <CartCard product={item} key={item._id} />;
+            })
+          ) : (
+            <div>
+              <p className="alert-container alert-error txt-medium">
+                No Products Found
+              </p>
+              <Link to="/products">
+                <button className="button btn-solid button-primary">
+                  Shop Now
+                </button>
+              </Link>
+            </div>
+          )}
+        </div>
+        <CartPrice />
+      </section>
+    </main>
+  );
 }
